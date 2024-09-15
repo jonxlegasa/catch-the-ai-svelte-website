924a76b15e4e3ad705c6b98ee732e93d4f4144db
Ethan <theethancastro@gmail.com>
2024-09-14 20:19:27 -0600

    Delete .svelte-kit directory

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/game/match/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/game/match/+page.svelte";
